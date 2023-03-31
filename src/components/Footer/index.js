7bade006477e827cc949a61ad8715e9c128fa2a7
zzeis <moisesmonteiro.stark@gmail.com>
2023-03-31 13:26:24 -0300

    add inf at footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,14 @@
 import './style.css';
 import {AiOutlineGithub} from 'react-icons/ai'
+import {MdLocationOn} from 'react-icons/md'
+import {BsFillTelephoneFill} from 'react-icons/bs'
 const Footer = () =>{
     return(
         <footer>
+             
+             <p className='adress'> <MdLocationOn/> Av. Princesa Isabel, 731 - Centro, Iguape - SP, 11920-000
+                 <BsFillTelephoneFill/>(13)997064784
+             </p>
             <p>by @zzeis <br/> <AiOutlineGithub/> </p>
             
         </footer>
