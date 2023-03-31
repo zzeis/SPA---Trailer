15984f23ddf9a2e83d6d2c90bef8c71d4c784be8
zzeis <moisesmonteiro.stark@gmail.com>
2023-03-31 16:10:43 -0300

    link git

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ const Footer = () =>{
              
              <span className='adress'>
                 
-                <MdLocationOn/> Av. Princesa Isabel , 731 - Centro, Iguape - SP (s/n)
+                <MdLocationOn/> Av. Princesa Isabel , 731 - Centro, Iguape - SP(s/n)
                 <BsFillTelephoneFill/>(13)997064784
              </span>
              <a href='https://github.com/zzeis/SPA---Trailer'> <p> by @zzeis <br/> <AiOutlineGithub/> </p></a>
